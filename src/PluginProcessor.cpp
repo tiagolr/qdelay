17cfdd912079f260b09cc921b7fe2955985c045c
tlr <tlcnr88@gmail.com>
2026-01-03 18:01:23 +0000

    attempt fix AU logic validation errors

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -334,7 +334,7 @@ bool QDelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) c
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
