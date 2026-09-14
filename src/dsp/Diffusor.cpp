555dcc7f7ccd52f94d077436737d7225859d5e91
tlr <tlcnr88@gmail.com>
2026-09-14 23:56:31 +0100

    prefer allpass instead of nested all pass

--- src/dsp/Diffusor.cpp
+++ src/dsp/Diffusor.cpp
@@ -11,6 +11,8 @@ int Diffusor::getNextPrime(int value)
 
 void Diffusor::clear()
 {
+	setSize(currSize, false);
+
 	for (int i = 0; i < NUM_ALLPASS / 2; i++)
 	{
 		allpassL[i].clear();
@@ -26,9 +28,10 @@ void Diffusor::prepare(float _srate)
 	{
 		modPhasesIncL[i] = (rand() / (float)RAND_MAX) * MOD_MAX_RATE / srateFactor / _srate;
 		modPhasesIncR[i] = (rand() / (float)RAND_MAX) * MOD_MAX_RATE / srateFactor / _srate;
+		allpassL[i].init(_srate);
+		allpassR[i].init(_srate);
 	}
 
-	setSize(currSize, false);
 	clear();
 }
 
@@ -44,14 +47,12 @@ void Diffusor::setSize(float size, bool smooth)
 		return val * pow(scale, (float)index);
 	};
 
-	for (int i = 0; i < NUM_ALLPASS / 2; ++i)
+	for (int i = 0; i < NUM_ALLPASS; ++i)
 	{
-		int sizeL1 = getNextPrime((int)scaleSize(std::floor(SIZE_L * size * 2), NUM_ALLPASS - 1 - i, scaleFactorL));
-		int sizeL2 = getNextPrime((int)scaleSize(std::floor(SIZE_L * size), NUM_ALLPASS - 1 - i, scaleFactorL));
-		int sizeR1 = getNextPrime((int)scaleSize(std::floor(SIZE_R * size * 2), NUM_ALLPASS - 1 - i, scaleFactorR));
-		int sizeR2 = getNextPrime((int)scaleSize(std::floor(SIZE_R * size), NUM_ALLPASS - 1 - i, scaleFactorR));
-		allpassL[i].setSize(sizeL1, sizeL2, smooth);
-		allpassR[i].setSize(sizeR1, sizeR2, smooth);
+		int sizeL = getNextPrime((int)scaleSize(std::floor(SIZE_L * size * 2), NUM_ALLPASS - 1 - i, scaleFactorL));
+		int sizeR = getNextPrime((int)scaleSize(std::floor(SIZE_R * size * 2), NUM_ALLPASS - 1 - i, scaleFactorR));
+		allpassL[i].setSize(sizeL, smooth);
+		allpassR[i].setSize(sizeR, smooth);
 	}
 }
 
@@ -62,23 +63,15 @@ void Diffusor::processBlock(float* left, float* right, int nsamps)
 		float spl0 = left[sample];
 		float spl1 = right[sample];
 
-		for (int i = 0; i < NUM_ALLPASS / 2; ++i) {
-			int i0 = i * 2;
-			int i1 = i * 2 + 1;
-			float mod00 = triangle(modPhasesL[i0]) * MOD_MAX_DEPTH;
-			float mod01 = triangle(modPhasesL[i1]) * MOD_MAX_DEPTH;
-			float mod10 = triangle(modPhasesR[i0]) * MOD_MAX_DEPTH;
-			float mod11 = triangle(modPhasesR[i1]) * MOD_MAX_DEPTH;
-			spl0 = allpassL[i].allPass(spl0, i % 2 == 0 ? smear : -smear, mod00, mod01);
-			spl1 = allpassR[i].allPass(spl1, i % 2 == 0 ? -smear : smear, mod10, mod11);
-			modPhasesL[i0] += modPhasesIncL[i0];
-			modPhasesL[i1] += modPhasesIncL[i1];
-			modPhasesR[i0] += modPhasesIncR[i0];
-			modPhasesR[i1] += modPhasesIncR[i1];
-			if (modPhasesL[i0] >= 1.f) modPhasesL[i0] -= 1.f;
-			if (modPhasesL[i1] >= 1.f) modPhasesL[i1] -= 1.f;
-			if (modPhasesR[i0] >= 1.f) modPhasesR[i0] -= 1.f;
-			if (modPhasesR[i1] >= 1.f) modPhasesR[i1] -= 1.f;
+		for (int i = 0; i < NUM_ALLPASS; ++i) {
+			float mod0 = triangle(modPhasesL[i]) * MOD_MAX_DEPTH;
+			float mod1 = triangle(modPhasesR[i]) * MOD_MAX_DEPTH;
+			spl0 = allpassL[i].allPass(spl0, i % 2 == 0 ? smear : -smear, mod0);
+			spl1 = allpassR[i].allPass(spl1, i % 2 == 0 ? -smear : smear, mod1);
+			modPhasesL[i] += modPhasesIncL[i];
+			modPhasesR[i] += modPhasesIncR[i];
+			if (modPhasesL[i] >= 1.f) modPhasesL[i] -= 1.f;
+			if (modPhasesR[i] >= 1.f) modPhasesR[i] -= 1.f;
 		}
 
 		left[sample] = spl0;
